f21aa6b6b99ca9f3146d7a8c2545ebe45448bddb
lucia <lucia@lucia-VirtualBox.(none)>
2013-03-05 01:23:47 +0100

    Import functions from functions.bash

--- run.bash
+++ run.bash
@@ -2,6 +2,8 @@
 scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 phpDir=`pear config-show 2> /dev/null | grep php_dir | awk '{print $NF}'`
 
+. $scriptDir/functions.bash
+
 function main {
 	parseConfig
 	$theCommand:$phpDir $scriptDir/PUWI_Runner.php $*
@@ -17,25 +19,6 @@ function parseConfig {
 	done < $scriptDir/config.ini
 }
 
-function checkEnterSection {
-	[[ "$line" =~ \[.*\] ]] && inDeps="no"
-}
 
-inDeps="no"
-
-function checkEnterDeps {
-	[ "$line" == "[dependencies]" ] && inDeps="yes"
-}
-
-function checkVarInLine {
-	sign=$( getWord "$1" 2 )
-	[ "$sign" != "" ] && [ "$inDeps" == "yes" ] && isDep="yes" || isDep="no"
-}
-
-isDep="no"
-
-function getWord {
-	echo $1 | awk '{print $'$2'}'
-}
 
 main $@
