ba5f21a7748d712ce537ae6b5f4692a36c859147
lucia <lucia@lucia-VirtualBox.(none)>
2013-03-14 17:52:16 +0100

    Change path of run instruction

--- launch_project.bash
+++ launch_project.bash
@@ -4,6 +4,12 @@
 scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 projectDir=`pwd`
 
-$scriptDir/run.bash $projectDir/tests/ 
+$scriptDir/run.bash $projectDir/
 
 echo -e " PUWI running... \n"
+
+
+
+
+
+
