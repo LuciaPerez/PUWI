8d1d683383c0f7850091023c896ac733357ff9f6
lucia <lucia@lucia-VirtualBox.(none)>
2013-09-20 20:07:36 +0200

    Avoid downloading the lastest PHPUnit version

--- load_deps.bash
+++ load_deps.bash
@@ -3,12 +3,8 @@ scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $scriptDir/bash/messages.bash
 
 
-[ -d $scriptDir/vendor ] || mkdir $scriptDir/vendor
-
 echo -e "Loading dependencies..."
 
-cd $scriptDir/vendor/
-git clone git://github.com/sebastianbergmann/phpunit.git
 
 cd phpunit
 curl -O http://getcomposer.org/composer.phar
