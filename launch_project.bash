6b59eef1f3915e02d533896bdd2e47ae28fa9741
lucia <lucia@lucia-VirtualBox.(none)>
2013-09-20 20:12:23 +0200

    Download always the same PHPUnit version: 3.8-gc07b181

--- launch_project.bash
+++ launch_project.bash
@@ -4,7 +4,12 @@
 scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 projectDir=`pwd`
 
-#$scriptDir/run.bash $projectDir/
+. $scriptDir/bash/functions.bash
+
+read_config
+
+change_owner $pubDirectory $projectDir
+
 
 x-www-browser http://localhost/PUWI/view/index.php?puwiDir=$scriptDir\&projectDir=$projectDir &
 
