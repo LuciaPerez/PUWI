c5d3acb0f2da96470e67a53130f13c2443b0e494
lucia <lucia@lucia-VirtualBox.(none)>
2013-09-20 16:26:20 +0200

    Search PUWI directory in the right path.

--- bash/functions.bash
+++ bash/functions.bash
@@ -24,29 +24,33 @@ function getWord {
 
 function change_owner {
 	mkdir tmp/
+	
 	echo $1 > tmp/tmp.txt
- 	
+
 	pubDirectory=`cut -d "/" -f4 tmp/tmp.txt`
 
+	
+	param=$1
 	length_dir=`expr length $pubDirectory`
-	total_length=`expr length $1`
+	total_length=`expr length $param`
+	
+	if [[ `echo ${param: -1}` = "/" ]]
+	then
+		total_length=`expr $total_length - 1`
+		param=`expr substr $param 1 $total_length`
+	fi
+
 	length_path_pub_dir=`expr $total_length - $length_dir`
 
-	path_pub_dir=`expr substr $1 1 $length_path_pub_dir`
-
+	path_pub_dir=`expr substr $param 1 $length_path_pub_dir`
+	
 	ls -l $path_pub_dir > tmp/permissions.txt
 
 	user=`awk '/'$pubDirectory'/ {print $0}' tmp/permissions.txt | cut -d " " -f4`
 
 	group=`awk '/'$pubDirectory'/ {print $0}' tmp/permissions.txt | cut -d " " -f5`
 
-
-	if [[ `expr substr $1 -1 1` = "/" ]]
-	then
-		path_puwi=$1"PUWI/"
-	else 
-		path_puwi=$1"/PUWI/"
-	fi
+	path_puwi=$param"/PUWI/"
 
 	chown $user $path_puwi
 	chgrp $group $path_puwi
