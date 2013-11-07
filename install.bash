67e424ac58dc9078d010e297dc5b1bed75cad224
Pepe Doval <pepellou@gmail.com>
2013-11-07 13:56:08 +0100

    Don't remove original repo on install

--- install.bash
+++ install.bash
@@ -19,7 +19,7 @@ function main {
 	addPathAutoload
 	$scriptDir/load_deps.bash 
 
-	mv $scriptDir/ $pubDirectory
+	cp -Rf $scriptDir/ $pubDirectory
 
 	if [[ `echo ${pubDirectory: -1}` = "/" ]]
 	then
